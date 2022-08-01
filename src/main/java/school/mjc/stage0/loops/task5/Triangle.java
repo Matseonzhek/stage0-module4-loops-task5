82d5fb30413c7957e6824dce905172a0bc41410a
Tanya Matseonzhek <matseonzhek@gmail.com>
2022-08-01 17:41:41 +0400

    Module4.2 task5

--- src/main/java/school/mjc/stage0/loops/task5/Triangle.java
+++ src/main/java/school/mjc/stage0/loops/task5/Triangle.java
@@ -1,7 +1,16 @@
 package school.mjc.stage0.loops.task5;
 
 public class Triangle {
-    public void printTriangle(int cathetusLength) {
+    public static void main(String[] args) {
+        new Triangle().printTriangle(5);
+    }
 
+    public void printTriangle(int cathetusLength) {
+        for (int i = cathetusLength; i > 0; i--) {
+            for (int j = i - 1; j < cathetusLength; j++) {
+                System.out.print(8);
+            }
+            System.out.println();
+        }
     }
 }
